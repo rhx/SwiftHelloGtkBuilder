6605b2bd2d2cf4493371f1263ef8dfb75336770a
Rene Hexel <r.hexel@griffith.edu.au>
2017-09-28 21:15:45 +1000

    Update for Swift 4

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
     name: "HelloGtkBuilder",
     dependencies: [
         .Package(url: "https://github.com/rhx/SwiftGtk.git", majorVersion: 3)
-    ]
+    ],
+    swiftLanguageVersions: [3, 4]
 )
