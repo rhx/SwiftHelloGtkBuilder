8d94368251b984a5700d39d9c863ad4d730717ab
Rene Hexel <r.hexel@griffith.edu.au>
2018-12-14 10:07:10 +1000

    Update dates

--- distclean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Remove Packages directory and generated files
-#
-. ./config.sh
-./clean.sh
-exec rm -rf Package.pins Packages Sources/${Module}.swift Sources/Swift${Mod}.swift ${Mod}.xcodeproj
