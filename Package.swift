561b9be4020ef4c8158777c3da571157bea8e282
Rene Hexel <r.hexel@griffith.edu.au>
2020-12-31 13:14:14 +1000

    s/master/main/

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "HelloGtkBuilder",
     dependencies: [
-        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("master")),
+        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("main")),
     ],
     targets: [
         .target(name: "HelloGtkBuilder", dependencies: ["Gtk"]),
